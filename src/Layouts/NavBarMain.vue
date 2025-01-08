a68925142519dd714fc52d43ae8c6d3182fcfa74
kshiitij <kshiitijsrivastava20@gmail.com>
2025-01-08 16:58:48 +0530

    home dit

--- src/Layouts/NavBarMain.vue
+++ src/Layouts/NavBarMain.vue
@@ -1,97 +1,104 @@
 <template>
-    <v-app-bar app color="white" dark class="button-bar" height="50" elevation="2">
-        <v-spacer></v-spacer>
-        
-        <v-divider :thickness="1" color="black" vertical></v-divider>
-        
-        <router-link to="/">
-            <v-btn :class="{'active': isActive('/')}" >
-                <v-icon icon="mdi-home" size="x-large"></v-icon> <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Home</div>
-            </v-btn>
-        </router-link>
-    
-        <v-divider :thickness="1" color="black" vertical></v-divider>
-    
-        <router-link to="/basin">
-            <v-btn :class="{'active': isActive('/basin')}">
-                <v-icon icon="mdi-leaf" size="x-large"></v-icon><div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Ganga Basin</div>
-            </v-btn>
-        </router-link>
-    
-        <v-divider :thickness="1" color="black" vertical></v-divider>
-    
-        <router-link to="/quality">
+<v-app-bar app color="white" dark class="button-bar" height="50" elevation="2">
+    <v-spacer></v-spacer>
+
+    <v-divider :thickness="1" color="black" vertical></v-divider>
+
+    <router-link to="/">
+        <v-btn :class="{'active': isActive('/')}">
+            <v-icon icon="mdi-home" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Home</div>
+        </v-btn>
+    </router-link>
+
+    <v-divider :thickness="1" color="black" vertical></v-divider>
+
+    <router-link to="/basin">
+        <v-btn :class="{'active': isActive('/basin')}">
+            <v-icon icon="mdi-leaf" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Ganga Basin</div>
+        </v-btn>
+    </router-link>
+
+    <v-divider :thickness="1" color="black" vertical></v-divider>
+
+    <router-link to="/quality">
         <v-btn :class="{'active': isActive('/quality')}">
-            <v-icon icon="mdi-water-check" size="x-large"></v-icon><div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Water Quality</div>
+            <v-icon icon="mdi-water-check" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">Water Quality</div>
         </v-btn>
-        </router-link>
-    
-        <v-divider :thickness="1" color="black" vertical></v-divider>
+    </router-link>
 
-        <router-link to="/flood">
+    <v-divider :thickness="1" color="black" vertical></v-divider>
+
+    <router-link to="/flood">
         <v-btn :class="{'active': isActive('/flood')}">
-            <v-icon icon="mdi-alert" size="x-large"></v-icon><div style="font-family: 'Poppins', sans-serif; font-weight: 400;"> Flood Hazard</div>
+            <v-icon icon="mdi-alert" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;"> Flood Hazard</div>
         </v-btn>
     </router-link>
 
-        <v-divider :thickness="1" color="black" vertical></v-divider>
+    <v-divider :thickness="1" color="black" vertical></v-divider>
 
-        <router-link to="/solid">
+    <router-link to="/solid">
         <v-btn :class="{'active': isActive('/solid')}">
-            <v-icon icon="mdi-recycle" size="x-large"></v-icon><div style="font-family: 'Poppins', sans-serif; font-weight: 400;"> Solid Waste</div>
+            <v-icon icon="mdi-recycle" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;"> Solid Waste</div>
         </v-btn>
     </router-link>
 
-        <v-divider :thickness="1" color="black" vertical></v-divider>
+    <v-divider :thickness="1" color="black" vertical></v-divider>
 
-        <router-link to="/about">
-            <v-btn :class="{'active': isActive('/about')}">
-                <v-icon icon="mdi-information" size="x-large"></v-icon> <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">About</div>
-            </v-btn>
-        </router-link>
+    <router-link to="/about">
+        <v-btn :class="{'active': isActive('/about')}">
+            <v-icon icon="mdi-information" size="x-large"></v-icon>
+            <div style="font-family: 'Poppins', sans-serif; font-weight: 400;">About</div>
+        </v-btn>
+    </router-link>
 
-        <v-divider :thickness="1" color="black" vertical></v-divider>
+    <v-divider :thickness="1" color="black" vertical></v-divider>
 
-        <v-img :src="nmcg"  max-width="130" max-height="50" class="image-hover"></v-img>
+    <v-img :src="nmcg" max-width="130" max-height="50" class="image-hover"></v-img>
 
-        <v-divider :thickness="1" color="black" vertical></v-divider>
+    <v-divider :thickness="1" color="black" vertical></v-divider>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+
+</v-app-bar>
+</template>
+
     
-    </v-app-bar>
-    </template>
     
-    <script>
-
-
-    export default {
-        name: 'NavBarMain',
-        data() {
-            return {
-                nmcg:require('@/assets/img/nmcgGif.gif'),     
-            }
-        },
-        methods: {
-            isActive(route) {
-                return this.$route.path === route;
-            }
+<script>
+export default {
+    name: 'NavBarMain',
+    data() {
+        return {
+            nmcg: require('@/assets/img/nmcgGif.gif'),
+        }
+    },
+    methods: {
+        isActive(route) {
+            return this.$route.path === route;
         }
     }
-    </script> 
+}
+</script> 
       
-    <style scoped>
-    .active {
-        height: 50px; 
-        background-color:  rgba(2, 42, 56,1);  
-        color: wheat;
-        font-size: 15px;       
-    }
-    .image-hover {
-      transition: 0.3s ease; 
-    }
-    .image-hover:hover {
-      transform: scale(1.1); 
-    }
-
-    </style>
     
+<style scoped>
+.active {
+    height: 50px;
+    background-color: rgba(2, 42, 56, 1);
+    color: wheat;
+    font-size: 15px;
+}
+
+.image-hover {
+    transition: 0.3s ease;
+}
+
+.image-hover:hover {
+    transform: scale(1.1);
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,22 +1,34 @@
 <template>
 <v-main>
     <div class="image-container">
-        <v-img :src="images[currentIndex]"/>
-
+        <v-img :src="images[currentImageIndex]" class="image"/>
         <v-btn @click="prevImage" icon class="left-arrow">
             <v-icon>mdi-chevron-left</v-icon> 
         </v-btn>
         <v-btn @click="nextImage" icon class="right-arrow">
             <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
-    </div>
-</v-main>
 
+        <v-scroll-x-reverse-transition>
+    <v-card :key="currentCardIndex" :prepend-icon="currentCard.icon" width="400" class="card">
+      <template v-slot:title>
+        <div class="d-flex justify-space-between align-center">
+          <span style="font-size: 21px; text-align: justify; font-family: 'Poppins', sans-serif; font-weight: bold;">{{ currentCard.title }}</span>
+            <v-icon @click="nextCard">mdi-chevron-right</v-icon>    
+        </div>
+      </template>
+      <v-divider></v-divider>
+      <v-card-text style="font-size: 14px; text-align: justify; font-family: 'Poppins', sans-serif; font-weight: 300;">
+        {{ currentCard.text }}
+      </v-card-text>
+    </v-card>
+</v-scroll-x-reverse-transition>
+
+</div>
+</v-main>
 <FooterHome />
 </template>
 
-  
-  
 <script>
 import FooterHome from '@/Layouts/FooterHome.vue';
 
@@ -28,31 +40,48 @@ export default {
                 require('@/assets/HomeImages/1.png'),
                 require('@/assets/HomeImages/2.png'),   
                 // Add more images here...
-            ],
-            currentIndex: 0, 
+                ],
+            currentImageIndex: 0, 
+
+            cards: [
+                    {title: "Ganga Basin", icon: "mdi-leaf", text: "Content about the Ganga Basin goes here. ",},
+                    {title: "Water Quality", icon: "mdi-water-check", text: "Content about water quality goes here.",},
+                    {title: "Flood Hazard", icon: "mdi-alert", text: "Content about flood hazard goes here.",},
+                    {title: "Solid Waste", icon: "mdi-recycle", text: "Content about solid waste management goes here.",},
+                ],
+            currentCardIndex: 0,
         };
+        
     },
     components: {
         FooterHome,
     },
+    computed: {
+    currentCard() {
+      return this.cards[this.currentCardIndex];
+    },
+  },
     methods: {
         prevImage() {
-            if (this.currentIndex > 0) {
-                this.currentIndex--;
+            if (this.currentImageIndex > 0) {
+                this.currentImageIndex--;
             } else {
-                this.currentIndex = this.images.length - 1; 
+                this.currentImageIndex = this.images.length - 1; 
             }
         },
         nextImage() {
-            if (this.currentIndex < this.images.length - 1) {
-                this.currentIndex++;
+            if (this.currentImageIndex < this.images.length - 1) {
+                this.currentImageIndex++;
             } else {
-                this.currentIndex = 0;
+                this.currentImageIndex = 0;
             }
         },
         startSlideshow() {
             this.interval = setInterval(() => {
                 this.nextImage(); }, 2000);
+        },
+        nextCard() {
+            this.currentCardIndex = (this.currentCardIndex + 1) % this.cards.length;
         },
     },
     mounted() {
@@ -60,8 +89,7 @@ export default {
     },
 };
 </script>
-  
-  
+
 <style scoped>
 .image-container {
     position: relative;
@@ -71,20 +99,24 @@ export default {
 .right-arrow {
     position: absolute;
     top: 50%;
-    transform: translateY(-50%);
-    z-index: 2;       
-}
-.left-arrow {
-    left: 10px;
-}
-.right-arrow {
-    right: 10px;
-}
-.v-btn {
+    /* transform: translateY(-50%); */
+    z-index: 2;  
     background-color: #022a38;
     color: wheat;
     border-radius: 50%;
-    opacity: 0.8;
+    opacity: 0.8;     
 }
-
+.left-arrow {
+    left: .5%;
+}
+.right-arrow {
+    right: .5%;
+}
+.card{
+    position: absolute;
+    top: 4% ;
+    right: 6%;
+    z-index: 2;
+    opacity: 0.8;  
+}
 </style>
