2fd312ebed0eab20add0aed2d1758cb179fa94c8
kshiitij7 <kshiitijsrivastava20@gmail.com>
2025-01-30 14:28:55 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-    }
-  },
-
-  devServer: {
-    host: '0.0.0.0',  // Allow external access to the app
-    port: 8081,        // Ensure the app runs on port 8081
-    disableHostCheck: true, // Disable host check for localhost/hostname access
-    public: 'geoganga.iirs.gov.in:8081',  // Access app via hostname
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
 })
